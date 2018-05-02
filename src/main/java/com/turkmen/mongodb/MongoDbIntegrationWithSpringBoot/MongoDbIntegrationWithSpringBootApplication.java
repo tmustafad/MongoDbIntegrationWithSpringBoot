b6c338b5e113d0dc03a51adcec226ac8fe7847b6
Turkmen Mustafa Demirci <tmustafad@gmail.com>
2018-05-02 14:23:14 +0300

    Initial project commit

---
+++ src/main/java/com/turkmen/mongodb/MongoDbIntegrationWithSpringBoot/MongoDbIntegrationWithSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.turkmen.mongodb.MongoDbIntegrationWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbIntegrationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbIntegrationWithSpringBootApplication.class, args);
+	}
+}
